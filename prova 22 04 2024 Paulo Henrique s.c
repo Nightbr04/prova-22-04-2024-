daa0b306fea005e6f2833fd94da61f4395eaf05e
Paulo H.S.C <148000460+Nightbr04@users.noreply.github.com>
2024-04-22 21:46:30 -0300

    Add files via upload

---
+++ prova 22 04 2024 Paulo Henrique s.c
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Área restrita</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Área restrita</h1>
+        <p>Para continuar, informe suas credencias abaixo ou saia da pagina:</p>
+        <form>
+            <label for="email">Digite seu E-mail:</label><br>
+            <input type="email" id="email" name="email"><br>
+            <label for="senha">Digite sua senha:</label><br>
+            <input type="password" id="senha" name="senha"><br>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
+ 
